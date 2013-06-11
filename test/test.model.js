c1866ae2ce94ffcea3a7aeb47293460a66386d2a
Matti Savolainen <matti@applifier.com>
2013-06-11 09:26:08 +0300

    progress

--- test/test.model.js
+++ test/test.model.js
@@ -1,10 +1,14 @@
 var Db = require('../');
 var assert = require('assert');
-
-
+var _ = require('underscore');
+var Backbone = require('backbone');
 
 describe('.sync()', function() {
   describe('when read should find from store', function(t) {
+    var m = new Backbone.Model({id:1});
+    m.sync = Db.sync;
+    m.save(null, {success: function() {
 
+    }});
   });
 });
