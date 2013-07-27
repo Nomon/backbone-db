a416ec894d48da5a2691efdb6f4b2b2842eebd17
Matti Savolainen <matti@applifier.com>
2013-07-27 22:58:28 +0300

    refactor

--- test/test.model.js
+++ test/test.model.js
@@ -3,12 +3,31 @@ var assert = require('assert');
 var _ = require('underscore');
 var Backbone = require('backbone');
 
-describe('.sync()', function() {
-  describe('when read should find from store', function(t) {
-    var m = new Backbone.Model({id:1});
-    m.sync = Db.sync;
-    m.save(null, {success: function() {
-
-    }});
+describe('Model', function() {
+  describe('#fetch', function(t) {
+    it('should .find from store', function(t) {
+      var m = new Backbone.Model({id:1});
+      Backbone.sync = Db.sync;
+      m.fetch({success: function() {
+        // id:1 is non existing
+      }, error: function() {
+        // so we get error
+        t();
+      }});
+    });
+    it('should include all the variables', function(t) {
+      var m = new Backbone.Model({id:1});
+      m.save({variable:"123"},{success: function() {
+        var m2 = new Backbone.Model({id:1});
+        m2.fetch({success: function() {
+          console.log(m2);
+          assert.equal(m2.get("variable"),"123");
+          t();
+        }, error: function(err) {
+          console.error(err);
+          assert.ok(false);
+        }});
+      }});
+    });
   });
 });
