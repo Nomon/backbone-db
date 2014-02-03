88dc08e22d05605e6d7cd13e15a8aaf49f9ca749
Matti Savolainen <matti@applifier.com>
2014-02-03 13:30:10 +0200

    Refactor tests so they can be used from other modules

--- test/test.model.js
+++ test/test.model.js
@@ -3,81 +3,107 @@ var assert = require('assert');
 var _ = require('lodash');
 var Backbone = require('backbone');
 
-var db = new Db("mymodel");
+module.exports = function (cb) {
+  after(function () {
 
-var MyModel = Backbone.Model.extend({
-  url: function() {
-    if(this.isNew())
-      return "mymodel";
-    return "mymodel:"+this.get(this.idAttribute);
-  },
-  sync: db.sync.bind(db)
-});
+    if (cb) cb();
+  });
 
-
-describe('Model', function() {
-  describe('#fetch', function(t) {
-    it('should .find from store', function(t) {
-      var m = new MyModel({id:1});
-      m.fetch({success: function() {
-        // id:1 is non existing
-      }, error: function() {
-        // so we get error
+  it('should .find from store', function (t) {
+    var m = new this.Model({});
+    m.fetch({
+      success: function () {
         t();
-      }});
-    });
-
-    it('should include all the variables', function(t) {
-      var m = new MyModel({id:1});
-      m.save({variable:"123", counter: 1}, {
-        success: function() {
-          var m2 = new MyModel({id:1});
-          m2.fetch({
-            success: function() {
-              assert.equal(m2.get("variable"), "123");
-              assert.equal(m2.get("counter"), 1);
-              t();
-            },
-            error: function(err) {
-              console.error(err);
-              assert.ok(false);
-            }
-          });
-        }
-      });
-    });
-
-    it('should inc counter', function(t) {
-      var m = new MyModel({id: 1});
-      var opts = {
-        inc: {
-          attribute: 'counter',
-          amount: 2
-        },
-        success: function() {
-          t();
-        },
-        error: function() {
-          console.error(err);
-          assert.ok(false);
-        }
-      };
-      m.save(null, opts);
-    });
-
-    it('should check that counter was incresed', function(t) {
-      var m2 = new MyModel({id:1});
-      m2.fetch({
-        success: function() {
-          assert.equal(m2.get("variable"), "123");
-          assert.equal(m2.get("counter"), 3);
-          t();
-        },
-        error: function(err) {
-          console.error(err);
-          assert.ok(false);
-        }
-      });
+      },
+      error: function () {
+        // so we get error
+      }
     });
   });
-});
+
+  it('should include all the variables', function (t) {
+    var Model = this.Model;
+    var m = new Model({
+      id: 1
+    });
+    m.save({
+      variable: "123",
+      counter: 1
+    }, {
+      success: function () {
+        var m2 = new Model({
+          id: 1
+        });
+        m2.fetch({
+          success: function () {
+            assert.equal(m2.get("variable"), "123");
+            assert.equal(m2.get("counter"), 1);
+            t();
+          },
+          error: function (err) {
+            console.error(err);
+            assert.ok(false);
+          }
+        });
+      }
+    });
+  });
+
+  it('should inc counter', function (t) {
+    var m = new this.Model({
+      id: 1
+    });
+    var opts = {
+      inc: {
+        attribute: 'counter',
+        amount: 2
+      },
+      success: function () {
+        t();
+      },
+      error: function () {
+        console.error(err);
+        assert.ok(false);
+      }
+    };
+    m.save(null, opts);
+  });
+
+  it('should check that counter was incresed', function (t) {
+    var m2 = new this.Model({
+      id: 1
+    });
+    m2.fetch({
+      success: function () {
+        assert.equal(m2.get("variable"), "123");
+        assert.equal(m2.get("counter"), 3);
+        t();
+      },
+      error: function (err) {
+        console.error(err);
+        assert.ok(false);
+      }
+    });
+  });
+  it('should remove the model', function (t) {
+    var m2 = new this.Model({
+      id: 1
+    });
+
+    m2.fetch({
+      success: function () {
+        assert.equal(m2.get("variable"), "123");
+        assert.equal(m2.get("counter"), 3);
+        m2.destroy({
+          success: function () {
+            t();
+          }
+        });
+      },
+      error: function (err) {
+        console.error(err);
+        assert.ok(false);
+      }
+    });
+  });
+};
